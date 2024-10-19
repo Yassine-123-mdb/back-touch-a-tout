9d3d620b3db67310bf83efb54505f4ac98730f84
Yassine-123-mdb <134618428+Yassine-123-mdb@users.noreply.github.com>
2024-10-19 23:52:26 +0200

    commit 1

---
+++ src/main/java/com/yassine/users/service/UserService.java
@@ -0,0 +1,16 @@
+package com.yassine.users.service;
+
+import java.util.List;
+
+import com.yassine.users.entities.Role;
+import com.yassine.users.entities.User;
+
+public interface UserService {
+	User saveUser(User user);
+	User findUserByUsername (String username);
+	Role addRole(Role role);
+	User addRoleToUser(String username, String rolename);
+	List<User> findAllUsers();
+
+
+}
