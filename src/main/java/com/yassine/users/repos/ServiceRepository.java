e7f33ff0402cd1db856900f929a844e1343ec25d
Yassine-123-mdb <134618428+Yassine-123-mdb@users.noreply.github.com>
2024-12-09 07:27:47 +0100

    login

--- src/main/java/com/yassine/users/repos/ServiceRepository.java
+++ src/main/java/com/yassine/users/repos/ServiceRepository.java
@@ -2,9 +2,11 @@ package com.yassine.users.repos;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.yassine.users.entities.Services;
+import com.yassine.users.entities.User;
 
 import java.util.List;
 
-public interface ServiceRepository extends JpaRepository<Services, Long> {
-    List<Services> findByUserId(Long userId); // Trouver tous les services d'un utilisateur
-}
+	public interface ServiceRepository extends JpaRepository<Services, Long> {
+	    List<Services> findByUser(User user); // Trouver les services en fonction de l'utilisateur
+	}
+
