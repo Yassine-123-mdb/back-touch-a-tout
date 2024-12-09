4f5ff539294a6e907c78af565b7279c273bb9488
Yassine-123-mdb <134618428+Yassine-123-mdb@users.noreply.github.com>
2024-12-09 02:05:27 +0100

    login

--- src/main/java/com/yassine/users/restControllers/UserRestController.java
+++ src/main/java/com/yassine/users/restControllers/UserRestController.java
@@ -22,7 +22,7 @@ import com.yassine.users.service.UserService;
 import com.yassine.users.service.register.RegistrationRequest;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST})
 public class UserRestController {
 	@Autowired
 	UserService userService;
